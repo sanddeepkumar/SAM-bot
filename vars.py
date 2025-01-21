b7e461c820e8b7aa293208a3ca39765d4531e79e
Sandeepa pattharia <sanju.kaminey@gmail.com>
2025-01-21 12:41:08 +0530

    Delete vars.py

--- vars.py
+++
@@ -1,5 +0,0 @@
-from os import environ
-
-API_ID = int(environ.get("API_ID", "26148784"))
-API_HASH = environ.get("API_HASH", "e1428799c0aeccd0a3cd1f15606e7a80")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
