674f79839cdc8dab0320b52db1482a6aa1979121
Sandeepa pattharia <sanju.kaminey@gmail.com>
2025-01-28 14:21:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @skpattharia 
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@sandeepa.pattharia 
+# Ask Doubt on telegram @skpattharia 
 
 import time
 import math
@@ -94,7 +94,7 @@ async def progress_bar(current, total, reply, start):
             progress_bar = "▰" * completed_length + "▱" * remaining_length
             
             try:
-                await reply.edit(f'<b>\n ╭──⌯════🆄︎ᴘʟᴏᴀᴅɪɴɢ⬆️⬆️═════⌯──╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n├🤖 𝔹ʏ » @VJ_Botz\n╰─═══ ✪ @VJ_Botz ✪ ═══─╯\n</b>') 
+                await reply.edit(f'<b>\n ╭──⌯════🆄︎ᴘʟᴏᴀᴅɪɴɢ═════⌯──╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n├🤖 𝔹ʏ »  @skpattharia\n╰─═══ ✪ @skpattharia ✪ ═══─╯\n</b>') 
             except FloodWait as e:
                 time.sleep(e.x)
 
